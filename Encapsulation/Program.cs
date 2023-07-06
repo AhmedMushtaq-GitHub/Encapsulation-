eeb8dd675798c38c44de2b2b0e51b85e6865d9af
AhmedMushtaq-GitHub <ahmed@AHMEDMUSHTAQ>
2023-07-06 22:27:12 +0500

    Add project files.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,8 @@
+﻿using Encapsulation;
+Console.WriteLine("Write your name then i will show your Belance");
+string input = Console.ReadLine();
+Security security = new Security();
+security.setName(input);
+security.setBalance(500000000);
+security.getName();
+security.getBalance();
---
+++ Encapsulation/Security.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Encapsulation
+{
+    internal class Security
+    {
+        private string name;
+        private int balance;
+
+       public void setName(string Name)
+        {
+            if (string.IsNullOrEmpty(Name) == true)
+            {
+                Console.WriteLine("invalid Name");
+                
+            }
+            else 
+            {
+                this.name = Name;
+            }
+           
+        }
+        public void getName()
+        {
+            Console.Write($"{ this.name} Your Belance is : ");
+        }
+
+        public void setBalance(int Balance) 
+        { 
+            this.balance = Balance;
+        }
+        public void getBalance()
+        {
+            if (string.IsNullOrEmpty(this.name) == true)
+            {
+                Console.WriteLine("input your name");
+
+            }
+            else
+            Console.Write(this.balance);
+            
+            
+        }
+    }
+}
